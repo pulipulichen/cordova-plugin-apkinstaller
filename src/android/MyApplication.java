e2fba2498c1c89660c98f5d9b173a611fc6893c3
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-07-08 00:08:28 +0800

    Update MyApplication.java

--- src/android/MyApplication.java
+++ src/android/MyApplication.java
@@ -2,7 +2,7 @@ package com.example.template;
 
 import android.app.Application;
 
-import com.mycompany.installer.CrashCatchHandler;
+//import com.mycompany.installer.CrashCatchHandler;
 
 public class MyApplication extends Application {
     public static final String TAG = "MyApplication";
